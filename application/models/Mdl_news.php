a1e13aa48b4962cba2b657c09b09654a78b2b467
Keerthivasan Sekar <s.keerthivasan7@gmail.com>
2019-01-20 10:34:07 +0530

    Added MY controller and model, Updated the same to login and news controllers

--- application/models/Mdl_news.php
+++ application/models/Mdl_news.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Mdl_news extends CI_Model
+class Mdl_news extends MY_Model
 {
 	
 	public function __construct()
--- application/models/Mdl_users.php
+++ application/models/Mdl_users.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Mdl_users extends CI_Model
+class Mdl_users extends MY_Model
 {
 	public function __construct()
 	{
